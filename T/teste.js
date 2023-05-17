b90e9c2e77bd7926376182df85e03303bb2d03ac
Renan Ferreira <100379663+firstbroken@users.noreply.github.com>
2023-05-17 14:59:56 -0300

    Add files via upload

---
+++ T/teste.js
@@ -0,0 +1,29 @@
+function logar(){
+
+let nome = document.getElementById("nome").value
+let senha = document.getElementById("senha").value
+
+let dados = [
+                {"id": Date.now,"nome": "will","senha": 1234}, //0
+                {"id": Date.now,"nome": "bob","senha": 2222}, //1
+                {"id": Date.now,"nome": "ringo","senha": 3333} //2
+            ]
+
+for(let i=0;dados.length > i;i ++){
+if(nome == dados[i].nome && senha == dados[i].senha){
+    alert("Está logado")
+    break
+}
+}
+}
+
+function teste(){
+    let item = ["zucas"]
+
+    let nomes = [["bruno", "ZAZ", "Mónica"] + "," + item]
+
+    localStorage.setItem("todos", nomes);
+
+    //Retrieve
+    document.getElementById("demo").innerHTML = localStorage.getItem("todos");
+}
